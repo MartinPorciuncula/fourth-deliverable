6ead6f416f84d039b20b6684615b5d32d0f12510
MartinPorciuncula <kgtprav@gmail.com>
2023-08-21 14:23:32 -0300

    visual bugs fixes and scrollbar styling,and modals animations

---
+++ src/services/createUsers.js
@@ -0,0 +1,29 @@
+export const userError = {
+    pattern: {
+        value: /[a-zA-Z]/,
+        message: 'Please type only letters (a-z, A-Z)'
+    },
+    minLength: {
+        value: 3,
+        message: 'Minimum 3 characters'
+    }
+}
+
+
+export const emailError = {
+    pattern: {
+        value: /\S+@\S+.\S+/,
+        message: 'Wrong email format'
+    }
+}
+
+export const paswwordError = {
+    minLength: {
+        value: 8,
+        message: 'minimum 8 characters'
+    },
+    maxLength: {
+        value: 16,
+        message: 'maximum 16 characters'
+    }
+}
